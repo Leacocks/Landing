2a3b123c019be4fe8e90bcb4a91503880fc90f07
Parker Moore <parkrmoore@gmail.com>
2012-10-13 19:50:13 -0400

    Allowing thumbnails from only one external site: leacocks.com

--- timthumb-config.php
+++ timthumb-config.php
@@ -5,8 +5,11 @@ define ('DEBUG_LEVEL', 1);						// Debug level 1 is less noisy and 3 is the most
 define ('DEBUG_LOG_FILE', 'timthumb.log');		// Log file
 
 define ('BLOCK_EXTERNAL_LEECHERS', false);		// If the image or webshot is being loaded on an external site, display a red "No Hotlinking" gif.
-define ('ALLOW_EXTERNAL', false);				// Allow image fetching from external websites. Will check against ALLOWED_SITES if
+define ('ALLOW_EXTERNAL', true);				// Allow image fetching from external websites. Will check against ALLOWED_SITES if
 												// ALLOW_ALL_EXTERNAL_SITES is false
+define ('ALLOW_ALL_EXTERNAL_SITES', false);
+
+$ALLOWED_SITES = array ('leacocks.com');
 
 define ('NOT_FOUND_IMAGE', '');					//Image to serve if any 404 occurs 
 define ('ERROR_IMAGE', '');						//Image to serve if an error occurs instead of showing error message
